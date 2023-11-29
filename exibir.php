96cd5d0570173860881822ac6f8e12885622b6a3
victor <victornascimento311@gmail.com>
2023-11-29 12:25:31 -0300

    Fazendo a incrementação do css na pagina de adoção

--- exibir.php
+++ exibir.php
@@ -7,12 +7,13 @@ $password = "";
 
 $mysqli = new mysqli($hostname, $username, $password, $database);
 
-if ($mysqli->connect_errno) {
-    die("Falha ao conectar: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error);
+// Verificar a conexão
+if ($mysqli->connect_error) {
+    die("Falha ao conectar: " . $mysqli->connect_error);
 }
 
 // Consulta SQL para obter os dados dos animais
-$query = "SELECT nome, raca, porte, microchip, nIMAGEM FROM animais";
+$query = "SELECT id, nome, raca, porte, microchip, nIMAGEM FROM animais";
 $resultado = $mysqli->query($query);
 
 // Iniciar a seção HTML
@@ -22,8 +23,10 @@ echo "<head>";
 echo "<meta charset='UTF-8'>";
 echo "<meta name='viewport' content='width=device-width, initial-scale=1.0'>";
 echo "<title>Animais para Adoção</title>";
+echo "<script src='https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js'></script>";
+echo "<link rel='stylesheet' href='C:\xampp\htdocs\Canil-Alpha\exibicao.css\'>";
 echo "</head>";
-echo "<body>";
+
 
 // Exibir os dados dos animais
 while ($row = $resultado->fetch_assoc()) {
@@ -33,14 +36,22 @@ while ($row = $resultado->fetch_assoc()) {
     echo "<p>Porte: {$row['porte']}</p>";
     echo "<p>Microchip: {$row['microchip']}</p>";
     
-    // Garantir que a extensão da imagem seja tratada corretamente
-    $extensao = pathinfo($row['nIMAGEM'], PATHINFO_EXTENSION);
+    // Imagem com link para mais informações
+    echo "<a href='detalhes_animal.php?id={$row['id']}'>";
     echo "<img src='imagem/{$row['nIMAGEM']}' alt='Foto do animal' style='max-width: 100%; height: auto;'>";
+    echo "</a>";
     
+    // Botão de adoção
+    echo "<form action='processar_adocao.php' method='post'>";
+    echo "<input type='hidden' name='animal_id' value='{$row['id']}'>";
+    echo "<input type='submit' value='Adotar'>";
+    echo "</form>";
+
     echo "</div>";
 }
 
 // Fechar a conexão e liberar os recursos
+$resultado->free_result();
 $mysqli->close();
 
 // Finalizar a seção HTML
